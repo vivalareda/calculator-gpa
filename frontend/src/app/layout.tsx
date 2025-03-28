5ff9e2ba1282769f0e0d4ad7db2a00837bd8d55a
Reda Tarzalt <tarzaltreda@gmail.com>
2025-03-28 12:31:18 -0400

    update favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: "500"
+  weight: "500",
 });
 
 export const metadata: Metadata = {
   title: "Calculateur de cote",
   description: "Calculer votre cote rapidement!",
+  icons: {
+    icon: [{ url: "/favicon.png", sizes: "32x32", type: "image/png" }],
+  },
 };
 
 export default function RootLayout({
